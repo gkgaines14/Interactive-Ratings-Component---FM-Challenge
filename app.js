b933b34d946d8a07c62545be0e4f87c0cec5c7e9
Greg Gaines <gkgaines@gmail.com>
2022-11-15 20:07:18 -0500

    added true functionality for cards

--- app.js
+++ app.js
@@ -1,14 +1,46 @@
-document.querySelector("#sub-button").onclick = function(){
-    console.log("button Clicked")
+let rating
+document.querySelector("#b-one").onclick = function(){
+    resetGlobes();
+    rating = '1'
+    document.querySelector("#b-one").className ="clicked globes";
 }
 
-document.querySelector("#b-one").onclick = function(){
-    let buttonOne = document.querySelector("#b-one");
-    if(buttonOne.className =="globes"){
-        buttonOne.className="clicked globes";
-    }else{
-        buttonOne.className="globes";
-    }
-    console.log("this is button 1")
-    console.log(buttonOne.className);
+document.querySelector("#b-two").onclick = function(){
+    resetGlobes();
+    rating = '2'
+    document.querySelector("#b-two").className ="clicked globes";
+}
+
+document.querySelector("#b-three").onclick = function(){
+    resetGlobes();
+    rating = '3'
+    document.querySelector("#b-three").className ="clicked globes";
+}
+
+document.querySelector("#b-four").onclick = function(){
+    resetGlobes();
+    rating = '4'
+    document.querySelector("#b-four").className ="clicked globes";
+}
+
+document.querySelector("#b-five").onclick = function(){
+    resetGlobes();
+    rating = '5'
+    document.querySelector("#b-five").className ="clicked globes";
+}
+
+
+function resetGlobes(){
+    document.querySelector('#b-one').className = 'globes'
+    document.querySelector('#b-two').className = 'globes'
+    document.querySelector('#b-three').className = 'globes'
+    document.querySelector('#b-four').className = 'globes'
+    document.querySelector('#b-five').className = 'globes'
+}
+
+document.querySelector("#sub-button").onclick = function(){
+    document.querySelector(".ratings-container").className = "hidden";
+    document.querySelector(".thankyou-container").className = "";
+
+    document.querySelector('#rating-value').innerHTML = rating;
 }
